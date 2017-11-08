b93972b94a53de92fb5e80dcc6fc70d62f6d403c
Steve <doublefly@live.cn>
2017-11-08 09:04:49 +0800

    d

---
+++ ChatRoom/ChatData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom
+{
+    public class ChatData
+    {
+        public int index { get; set; }
+    }
+}
